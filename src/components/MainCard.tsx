6c9d535330e8a5f54f25e8fb1b93f081f2a806cc
VarGun <heegun46900@gmail.com>
2025-05-14 09:56:55 +0900

    svg component 및 transition 추가

---
+++ src/components/MainCard.tsx
@@ -0,0 +1,30 @@
+import type { ReactNode } from 'react';
+import Check from '../assets/Check.tsx';
+type Props = {
+  selected: boolean;
+  content: string;
+  children: ReactNode;
+};
+const MainCard = ({ selected, content, children }: Props) => {
+  return (
+    <div
+      className={`
+    w-1/2 flex flex-col items-center justify-end gap-8 pt-20 pb-12 rounded-xl
+    border-4 border-[var(--color-primary)] relative transition-colors duration-300
+    ${
+      selected
+        ? 'bg-[var(--color-primary)] text-white'
+        : 'bg-white text-[var(--color-primary)] hover:bg-[var(--color-primary)] hover:text-white'
+    }
+  `}
+    >
+      {children}
+      <div className={`text-2xl font-bold`}>{content}</div>
+      <div className="absolute top-2 right-2 z-10 overflow-hidden">
+        <Check />
+      </div>
+    </div>
+  );
+};
+
+export default MainCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,30 @@
+import MainCard from '../components/MainCard';
+import EnterIcon from '../assets/EnterIcon.tsx';
+import AddIcon from '../assets/AddIcon.tsx';
+
+import { useState } from 'react';
+
 const HomePage = () => {
+  const [mode, setMode] = useState<boolean>(true);
+
+  const toggleTest = () => {
+    console.log('mode', mode);
+    setMode(!mode);
+  };
+
   return (
-    <div className=" w-full flex items-center justify-center border-blue-950 border-2">
-      <div></div>
+    <div className="w-full flex items-center justify-center pt-20 border-red-500 border-2">
+      <div className="w-3/5 flex flex-col gap-10 border-yellow-500 border-2">
+        <button onClick={() => toggleTest()}>Toggle Test</button>
+        <div className="flex gap-10">
+          <MainCard selected={mode} content="방 만들기">
+            <AddIcon />
+          </MainCard>
+          <MainCard selected={!mode} content="참여하기">
+            <EnterIcon />
+          </MainCard>
+        </div>
+      </div>
     </div>
   );
 };
