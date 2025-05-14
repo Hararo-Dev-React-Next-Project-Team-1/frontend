1a55c2d76740fe79565b8a2dd22e5a2d0e2ebda7
VarGun <heegun46900@gmail.com>
2025-05-14 18:48:04 +0900

    user chat input 구현

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,6 +1,22 @@
 // Header Test Page
 import RoomHeader from '../components/RoomHeader';
+import ChatInput from '../components/ChatInput.tsx';
+import { useEffect, useState } from 'react';
 const TestPage = () => {
+  const [userChat, setUserChat] = useState('');
+
+  useEffect(() => {
+    console.log('userChat : ', userChat);
+  }, [userChat]);
+
+  const sendChat = () => {
+    if (userChat.length == 0) {
+      return;
+    }
+    // Todo : 채팅 추가 기능
+    console.log(userChat);
+    setUserChat('');
+  };
   return (
     <div className="w-full flex items-center justify-center py-20">
       <div
@@ -9,6 +25,7 @@ const TestPage = () => {
       >
         <RoomHeader title={'Hooks 파헤치기'} roomCode={1234} />
         <h1 className="text-4xl font-bold">Router Test Page</h1>
+        <ChatInput onChange={setUserChat} sendChat={sendChat} />
       </div>
     </div>
   );
