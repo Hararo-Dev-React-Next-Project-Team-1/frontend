7cabbe325bd0470cb22817658b3cfee0073baafc
VarGun <heegun46900@gmail.com>
2025-05-14 17:42:10 +0900

    room header 개발 중

---
+++ src/components/RoomHeader.tsx
@@ -0,0 +1,31 @@
+type Props = {
+  title: string;
+  roomCode: number;
+};
+
+const RoomHeader = ({ title, roomCode }: Props) => {
+  const date = new Date();
+  const formatted = date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  });
+
+  return (
+    <div className="w-full py-4 px-1 flex justify-between items-end border-b-2 border-[#CFCFCF]">
+      <div className="flex flex-col gap-1 justify-center text-[var(--color-gray-2)]">
+        <span className="text-sm">Date</span>
+        <div className="font-semibold text-xl">{formatted}</div>
+      </div>
+      <span className="text-2xl font-semibold">{title}</span>
+      <div className="flex flex-col gap-1 justify-center items-end text-[var(--color-gray-2)]">
+        <span className="text-sm">Enter Code</span>
+        <div className="font-semibold text-xl text-[var(--color-primary)]">
+          #{roomCode}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RoomHeader;
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,7 +1,14 @@
+// Header Test Page
+import RoomHeader from '../components/RoomHeader';
 const TestPage = () => {
   return (
-    <div className="container">
-      <h1 className="text-4xl font-bold">Router Test Page</h1>
+    <div className="w-full flex items-center justify-center py-20 border-2 border-red-700">
+      <div
+        className="w-3/5 flex items-center justify-center
+      border-2 border-[var(--color-gray)]"
+      >
+        <RoomHeader title={'Hooks 파헤치기'} roomCode={1234} />
+      </div>
     </div>
   );
 };
