f80b752b068103f23702f87f39846fec35e8e026
VarGun <heegun46900@gmail.com>
2025-05-14 19:09:31 +0900

    Sorting & Question Count 구현

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,9 +1,12 @@
 // Header Test Page
 import RoomHeader from '../components/RoomHeader';
 import ChatInput from '../components/ChatInput.tsx';
+import Sorting from '../assets/Sorting.svg?react';
+
 import { useEffect, useState } from 'react';
 const TestPage = () => {
   const [userChat, setUserChat] = useState('');
+  const [QCount, setQCount] = useState(2);
 
   useEffect(() => {
     console.log('userChat : ', userChat);
@@ -26,6 +29,16 @@ const TestPage = () => {
         <RoomHeader title={'Hooks 파헤치기'} roomCode={1234} />
         <h1 className="text-4xl font-bold">Router Test Page</h1>
         <ChatInput onChange={setUserChat} sendChat={sendChat} />
+        <div className="w-full flex justify-between items-center text-[16px] text-[#737373]">
+          <div
+            className="rounded-2xl flex items-center relative
+          px-16 py-4   font-medium border border-[#CFCFCF]"
+          >
+            <Sorting className="absolute left-7" />
+            <span>Recent</span>
+          </div>
+          <span>{QCount} Questions</span>
+        </div>
       </div>
     </div>
   );
