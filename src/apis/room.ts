30651a32993274761351baaab5b0cf9550f2f7c9
VarGun <heegun46900@gmail.com>
2025-05-15 21:32:11 +0900

    test API 및 createroom 수정

--- src/apis/room.ts
+++ src/apis/room.ts
@@ -1,7 +1,27 @@
 import axios from './axiosInstance';
-export const createRoom = (title: string, file: string | File) => {
-  axios.post('/rooms', {
-    title: title,
-    file: file,
-  });
+export const createRoom = async (title: string, file: string | File) => {
+  const formData = new FormData();
+  formData.append('title', title);
+
+  if (file instanceof File) {
+    formData.append('file', file);
+  }
+
+  try {
+    const response = await axios.post('/rooms', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('error:', error);
+    // throw error;
+  }
+};
+
+export const testAPI = async (roomId: string) => {
+  const response = await axios.get('/room/' + roomId);
+  return response.data;
 };
