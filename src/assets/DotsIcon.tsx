5538e042ac4b7a90b5bac56ad2b7975a29db7d80
youlimsongs <giuhki@gmail.com>
2025-05-16 13:04:07 +0900

    chore: 아이콘파일 속성명 변경

--- src/assets/DotsIcon.tsx
+++ src/assets/DotsIcon.tsx
@@ -3,15 +3,15 @@ export const DotsIcon = () => {
     <svg
       data-slot="icon"
       fill="none"
-      stroke-width="1"
+      strokeWidth="1"
       stroke="currentColor"
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
       aria-hidden="true"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
       ></path>
     </svg>
